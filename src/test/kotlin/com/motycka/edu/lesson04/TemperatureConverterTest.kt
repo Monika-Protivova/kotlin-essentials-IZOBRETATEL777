4bfe0a7aa7d4152b85c2b03c56b4f9a601562caa
Roman Tolstosheyev <rtolstosheyev@intershop.com>
2025-06-11 00:31:14 +0400

    feat: implement lesson 4

--- src/test/kotlin/com/motycka/edu/lesson04/TemperatureConverterTest.kt
+++ src/test/kotlin/com/motycka/edu/lesson04/TemperatureConverterTest.kt
@@ -1,11 +1,25 @@
 package com.motycka.edu.lesson04
 
 import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.doubles.plusOrMinus
+import io.kotest.matchers.doubles.shouldBeExactly
 import io.kotest.matchers.shouldBe
 
-// By convention, the name of the test class should be the name of the class under test + "Test"
 class TemperatureConverterTest : StringSpec({
 
-    // TODO Implement tests for the TemperatureConverter class
+    "should convert 32°F to 0°C" {
+        TemperatureConverter.toCelsius(32.0) shouldBeExactly 0.0
+    }
 
+    "should convert 212°F to 100°C" {
+        TemperatureConverter.toCelsius(212.0) shouldBeExactly 100.0
+    }
+
+    "should convert 98.6°F to approximately 37°C" {
+        TemperatureConverter.toCelsius(98.6) shouldBe(37.0 plusOrMinus 0.0001)
+    }
+
+    "should convert -40°F to -40°C" {
+        TemperatureConverter.toCelsius(-40.0) shouldBeExactly -40.0
+    }
 })
